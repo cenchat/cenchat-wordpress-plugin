824d23455ca6a25f137ce8aa75ed4e01a45cc886
Mikko Paderes <rmmpaderes@gmail.com>
2018-08-05 15:40:17 +0800

    Update wording in admin settings

--- admin/partials/cenchat-comments-general-settings-section-header.php
+++ admin/partials/cenchat-comments-general-settings-section-header.php
@@ -1,5 +1,1 @@
-<p>
-    Cenchat for publishers is currently available to select partners only.
-    <br>
-    To get your Cenchat ID, you may request an invite at <a href="https://cenchat.com" target="_blank">cenchat.com</a>.
-</p>
+<p>To get your Cenchat ID, sign up and register your website at <a href="https://cenchat.com" target="_blank">cenchat.com</a></p>
